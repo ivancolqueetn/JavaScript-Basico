21304f91002085b8583e2ea0cd7c9b66f8d44843
ivancolqueetn <77028322+ivancolqueetn@users.noreply.github.com>
2023-06-23 11:37:16 -0400

    Update and rename iniciacion-notacion.js to ejercicio2.js

---
+++ sesion-01-02-mi-primer-proyecto-de-node/ejercicio2.js
@@ -0,0 +1,11 @@
+var nombre = "ivan";
+var edad = 30;
+var desarrollador = false;
+var fechaNacimiento = new Date(1992,9,7);
+var libro={
+    nombre:"el milagro mas grande del mundo",
+    autor:"Og Mandino",
+    año:1976,
+    url:'https://www.casadellibro.com/libro-el-milagro-mas-grande-del-mundo/9788497938297/1065689'
+}
+
